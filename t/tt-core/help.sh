394b7301360507c89dede82ff5bf3cea859da55c
Zachary Zhou <zchrzhou@gmail.com>
2024-09-10 11:44:56 +0800

    Update okapi for import

--- t/tt-core/help.sh
+++ t/tt-core/help.sh
@@ -2,5 +2,9 @@
 
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 CORE_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
+CORE_TOP=$(cd $CORE_ROOT/.. && pwd)
 
-echo "env CORE_ROOT: $CORE_ROOT"
+cat <<EOF
+CORE_ROOT: $CORE_ROOT
+CORE_TOP: $CORE_TOP
+EOF
